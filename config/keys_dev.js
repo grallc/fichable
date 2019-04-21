596964c304ad915e3cb118c3fd3649902ab21dd3
Corentin Grall <corentin.grall@gmail.com>
2019-04-21 19:36:36 +0200

    Moved Firebase to FrontEnd - Deleted config folder :art::fire:

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyBvPlLMs4diePZNleDq4LgM-8fMUazrgnI",
-    authDomain: "fichable-dev.firebaseapp.com",
-    databaseURL: "https://fichable-dev.firebaseio.com",
-    projectId: "fichable-dev",
-    storageBucket: "fichable-dev.appspot.com",
-    messagingSenderId: "327394286391"
-};
--- config/keys_prod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apiKey: "yourFirebaseApiKey",
-    authDomain: "yourFirebaseAuthDomain",
-    databaseURL: "yourFirebaseDatabaseUrl",
-    projectId: "yourFirebaseProjectId",
-    storageBucket: "yourFirebaseStorageBucket",
-    messagingSenderId: "yourFirebaseMessagingSenderId"
-};
