5b14142e4dca485207c9ee1a6b9dc9519863f564
Corentin Grall <corentin.grall@gmail.com>
2019-05-03 23:02:52 +0200

    Re-structured the code :art:

--- app.js
+++ app.js
@@ -25,44 +25,29 @@ mongoose
     .catch(err => console.log(err));
 
 // Partials' folder
-hbs.registerPartials(__dirname + "/views/layout/partials");
-
-hbs.registerHelper('allPosts', () => {
-    Post.find({}).then((posts) => {
-        console.log('1234')
-        return posts;
-    }, (e) => {
-        console.log('123');
-    });
-});
-
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Define Handlebars as the main view engine
 app.set("view engine", "hbs");
 
-// Where are views located ?
-app.use(express.static(__dirname + "/public"));
-
 // index.html page
 app.get("/", function (req, res) {
     Post.find({}).then((posts) => {
-        console.log(posts);
-        res.render(path.join(__dirname + "/views/layout/index.hbs"), {posts});
+        res.render("index", {posts});
     }, (e) => {
-        console.log(e.message)
+        res.render("index", {postsError : e.message})
     });
     
 });
 
-
 // create.html page
 app.get('/create', (req, res) => {
-    res.render(path.join(__dirname + "/views/layout/create.hbs"));
+    res.render("create");
 });
 
 // profile.html page
 app.get('/profile', (req, res) => {
-    res.render(path.join(__dirname + "/views/layout/profile.hbs"));
+    res.render("profile");
 });
 
 
