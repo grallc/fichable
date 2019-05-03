fc6975d3c82037f16762e5ae58be25a084db2859
Corentin Grall <corentin.grall@gmail.com>
2019-05-03 22:55:56 +0200

    Posts are now displaying :sparkles::construction:

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require("path");
 const hbs = require("hbs");
 const fs = require("fs");
+
 const mongoose = require('mongoose');
+var { Post } = require('./models/post');
 
 // Application the port run in
 const port = 8080;
@@ -16,13 +18,24 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
-  .then(() => console.log(`MongoDB Connected`))
-  .catch(err => console.log(err));
-
+    .connect(mongoURI, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log(`MongoDB Connected`))
+    .catch(err => console.log(err));
 
 // Partials' folder
 hbs.registerPartials(__dirname + "/views/layout/partials");
+
+hbs.registerHelper('allPosts', () => {
+    Post.find({}).then((posts) => {
+        console.log('1234')
+        return posts;
+    }, (e) => {
+        console.log('123');
+    });
+});
+
 
 // Define Handlebars as the main view engine
 app.set("view engine", "hbs");
@@ -31,9 +44,16 @@ app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 // index.html page
-app.get("/", function(req, res) {
-    res.render(path.join(__dirname + "/views/layout/index.hbs"));
+app.get("/", function (req, res) {
+    Post.find({}).then((posts) => {
+        console.log(posts);
+        res.render(path.join(__dirname + "/views/layout/index.hbs"), {posts});
+    }, (e) => {
+        console.log(e.message)
+    });
+    
 });
+
 
 // create.html page
 app.get('/create', (req, res) => {
@@ -50,4 +70,3 @@ app.get('/profile', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
-
